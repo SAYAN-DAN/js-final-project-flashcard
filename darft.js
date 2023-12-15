98db48ecbf01c5df9d0051ea3d9756b42547fc8b
SAYAN-DAN <sayan94.arambagh@gmail.com>
2023-12-15 20:18:28 +0530

    Add Some Commit

--- darft.js
+++ darft.js
@@ -4,18 +4,21 @@ const sum = sumArray([1, 2, 3, 4, 5]);
 
 console.log(sum); //* Output: 15
 
+// *******************************************************************************************
 //? Problem 2
 
 const maxNumber = findMaxNumber([10, 5, 8, 20, 3]);
 
 console.log(maxNumber); //* Output: 20
 
+// *******************************************************************************************
 //? Problem 3
 
 const oddNumbers = filterOddNumbers([1, 2, 3, 4, 5]);
 
 console.log(oddNumbers); //* Output: [1, 3, 5]
 
+// *******************************************************************************************
 //? Problem 4
 
 const isPalindromeResult = isPalindrome('radar');
@@ -24,12 +27,14 @@ const isNonPalindromeResult = isPalindrome('hello');
 console.log(isPalindromeResult); //* Output: true
 console.log(isNonPalindromeResult); //* Output: false
 
+// *******************************************************************************************
 //? Problem 5
 
 const missingNumber = findMissingNumber([1, 2, 3, 5, 6]);
 
 console.log(missingNumber); //* Output: 4
 
+// *******************************************************************************************
 //? Problem 6
 
 const duplicateNumbers = findDuplicates([1, 2, 3, 2, 4, 5, 4, 6]);
